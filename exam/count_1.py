e954241d3f1dbaf87d5208c244186104ffce73b4
sun123zhengjun <13721232197@163.com>
2017-09-02 20:27:19 -0700

    count number of 1

--- exam/count_1.py
+++ exam/count_1.py
@@ -1,19 +1,14 @@
 #coding:utf-8
 """
-输出数字的二进制中的1的个数; 判断一个数是否为2的幂
+输入一个整数n，求从1到n这n个正数中，1出现的次数。例如：输入12，出现一的数字有1，10，11，12共有5个1，则输出5.
 """
-def count_1(n):
-	counter = 0
-	while n:
-		n = n&(n-1)
-		counter +=1
-	return counter
+def count_number_one(n):
+	count = 0
+	for i in range(n+1):
+		while i:
+			if i % 10 == 1:
+				count += 1
+			i = i/10
+	return count
+print(count_number_one(12))
 
-def power_2(n):
-	if n&(n-1) == 0:
-		return True
-	else:
-		return False
-
-print(count_1(15))
-print(power_2(8))
